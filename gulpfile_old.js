252527f94d7df29c9ea30569c1105e965a90ccf3
Pomis <pomis172@gmail.com>
2016-05-24 21:07:42 +0300

    временный коммит чтобы сдать лабу

--- gulpfile_old.js
+++ gulpfile_old.js
@@ -52,6 +52,7 @@
 var gulp = require('gulp'),				// Gulp
 	jade = require('gulp-jade'), 		// шаблонизатор для html
 	sass = require('gulp-sass'), 		// препроцессор css
+	stylus = require('gulp-stylus'),	// препроцессор css
 	connect = require('gulp-connect'); 	// локальный сервер для быстрого просмотра имзменений в файлах
 
 // server
@@ -78,6 +79,13 @@ gulp.task('sass', function() {
 		.pipe(connect.reload());
 });
 
+// stylus
+gulp.task('stylus', function() {
+	return gulp.src('src/styles/*.styl')
+    	.pipe(stylus({compress: true}))
+    	.pipe(gulp.dest('build/styles/'))//,
+    	//.pipe(connect.reload());
+});
 
 // js
 gulp.task('js', function() {
@@ -88,10 +96,11 @@ gulp.task('js', function() {
 gulp.task('watch', function() {
 	gulp.watch('src/*.jade', ['jade']);
 	gulp.watch('src/styles/*.scss', ['sass']);
+	gulp.watch('src/styles/*.styl', ['stylus']);
 	gulp.watch('src/scripts/*.js', ['js']);
 
 });
 
 gulp.task('default', function() {
-	gulp.start('connect', 'watch', 'jade', 'sass', 'js');
+	gulp.start('connect', 'watch', 'jade', 'sass', 'stylus', 'js');
 });
