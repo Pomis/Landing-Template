2da977ae47ba1870870e7e8901fff6610ff7514a
Pomis <pomis172@gmail.com>
2016-05-25 11:16:07 +0300

    windows installer

---
+++ install.bat
@@ -0,0 +1,10 @@
+call npm install --save-dev gulp
+call npm install --save-dev gulp-sass
+call npm install --save-dev gulp-jade
+call npm install --save-dev gulp-stylus
+call npm install --save-dev gulp-connect
+mkdir build
+mkdir build/styles
+mkdir build/fonts
+mkdir build/images
+mkdir build/scripts
